79a4e0c0c124f5082cda330fd3f90109e2a44050
hasha2982 <hashadev@yandex.ru>
2023-02-08 16:25:36 +0300

    Add __main__.py

---
+++ src/__main__.py
@@ -0,0 +1,12 @@
+"""
+Maze generator and solver written in Python
+Use mazegen -h for help.
+"""
+
+import argparse
+
+parser = argparse.ArgumentParser(
+    prog="mazegen",
+    description="Maze generator and solver written in Python"
+)
+parser.parse_args()
