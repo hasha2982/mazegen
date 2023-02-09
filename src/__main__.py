4fe5500d7ae4035ab676eebda567615c8625d7d0
hasha2982 <hashadev@yandex.ru>
2023-02-09 21:05:35 +0300

    Add Maze class + change main

--- src/__main__.py
+++ src/__main__.py
@@ -5,17 +5,28 @@ Use mazegen -h for help.
 
 import argparse
 
+LATEST_MANIFEST = 0 # Latest supported manifest version. Use -V to ignore
+
 parser = argparse.ArgumentParser(
     prog="mazegen",
     description="Maze generator and solver written in Python"
 )
+
+# All
 parser.add_argument(
     "-m", "--mode",
     help="choose a mode (render/generate/solve)"
 )
+
+# Render/solve
 parser.add_argument(
     "-f", "--file",
-    help=".json maze file to solve/render"
+    help=".json maze file to solve/render (only render/solve)"
+)
+parser.add_argument(
+    "-V", "--ignore-version",
+    help="ignore the maze json file version. might cause errors (only render/solve)",
+    action='store_true'
 )
 
 # Render args
