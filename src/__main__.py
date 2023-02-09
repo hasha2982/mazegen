0e957aa4eb4a9102302d63ac4ffdbe47e88bba65
hasha2982 <hashadev@yandex.ru>
2023-02-09 15:08:47 +0300

    Add more flags to main

--- src/__main__.py
+++ src/__main__.py
@@ -9,4 +9,19 @@ parser = argparse.ArgumentParser(
     prog="mazegen",
     description="Maze generator and solver written in Python"
 )
+parser.add_argument(
+    "-m", "--mode",
+    help="choose a mode (render/generate/solve)"
+)
+parser.add_argument(
+    "-f", "--file",
+    help=".json maze file to solve/render"
+)
+
+# Render args
+parser.add_argument(
+    "-r", "--renderer",
+    help="rendering method"
+)
+
 parser.parse_args()
