3aca9dbe861065e09048d94064cde4c3f0688131
hasha2982 <hashadev@yandex.ru>
2023-02-24 13:42:05 +0300

    Backup commit

--- __main__.py
+++ __main__.py
@@ -4,7 +4,10 @@ Use mazegen -h for help.
 """
 
 import argparse
-import sys
+import os
+import textwrap
+import importlib.util
+import pathlib
 
 import colorama as co
 
@@ -12,13 +15,22 @@ import colorama as co
 from lib.logging import formatter as custom_formatter # pylint: disable=import-error,no-name-in-module
 # FIXME: Edit .vscode to extend the import path
 
-co.init()
+co.init() # Init colorama
 
-LATEST_MANIFEST = 0 # Latest supported manifest version. Use -V to ignore
+LATEST_MANIFEST = 0 # Latest supported manifest version.
 
 parser = argparse.ArgumentParser(
     prog="mazegen",
-    description="Maze generator and solver written in Python"
+    formatter_class=argparse.RawDescriptionHelpFormatter,
+    description=textwrap.dedent("""
+    Maze generator and solver written in Python.
+    Modes:
+    list | l
+        list all available generators, solvers and renderers
+    
+    render | r
+        render .json maze file
+    """)
 )
 
 # Logging
@@ -30,9 +42,13 @@ parser.add_argument(
 )
 
 # All
+# parser.add_argument(
+#     "-m", "--mode",
+#     type=str,
+#     help="choose a mode (render/generate/solve)"
+# )
 parser.add_argument(
-    "-m", "--mode",
-    type=str,
+    "mode",
     help="choose a mode (render/generate/solve)"
 )
 
@@ -51,6 +67,11 @@ parser.add_argument(
 parser.add_argument(
     "-r", "--renderer",
     help="rendering method"
+)
+
+parser.add_argument(
+    "-A", "--renderer-args",
+    help="additional arguments for the renderer. check the renderer docs for supported renderer args."
 )
 
 args = parser.parse_args()
@@ -80,14 +101,57 @@ match args.verbose.upper(): # the type of flag is str, so we're sure it has .upp
 # l.error("Error")
 # l.critical("Critical")
 
+def list_renderers():
+    """
+    Get all .py files in /renderers/ and check if they can be used as renderers
+    """
+    renderers_dir = "renderers"
+
+    # List of all found .py files
+    py_files_list = []
+
+    # Get all files and dirs in dir
+    for filename in os.listdir(renderers_dir):
+        l.debug("Analyzing %s", filename)
+        f = os.path.join(renderers_dir, filename)
+
+        #print(f)
+        if os.path.isfile(f) and f.lower().endswith(".py"):
+            py_files_list.append(f)
+            l.debug("%s is a .py file, appending to list", f)
+
+    # Try to import and check them
+    for file in py_files_list:
+        # Try to extract stem from path
+        name = pathlib.Path(file).stem
+
+        # Import module
+        try:
+            spec = importlib.util.spec_from_file_location(name, file)
+            module = importlib.util.module_from_spec(spec)
+            spec.loader.exec_module(module)
+            l.debug("Successfully imported %s from %s", name, file)
+        except Exception as e: # pylint: disable=broad-exception-caught
+            l.debug("Can't import %s from %s: unknown error (%s). Skipping", name, file, e)
+            continue
+
+        try:
+            factory = module.RendererFactory()
+        except AttributeError as e:
+            l.debug("Imported module has no RendererFactory. Skipping (%s)", e)
+
 if __name__ == "__main__":
-    if args.mode is None:
-        l.critical("Missing mode argument. Try -h or --help")
-        sys.exit(1)
-    
+    ## Not required now since mode is positional
+    # if args.mode is None:
+    #     l.critical("Missing mode argument. Try -h or --help")
+    #     sys.exit(1)
+
     match args.mode.lower():
+        case "list" | "l":
+            l.debug("list mode")
+            list_renderers()
         case "render" | "r":
-            l.debug("render")
+            l.debug("render mode")
         case _:
-            l.critical("Unknown mode: %s. Supported modes: render. Try -h or --help", args.mode)
+            l.critical("Unknown mode: %s. Supported modes: render, list. Try -h or --help", args.mode)
     
--- renderers/base.py
+++ renderers/base.py
@@ -2,12 +2,32 @@
 This module contains a base renderer for mazegen.
 """
 
+from ..lib import maze
+
 class BaseRenderer:
     """
     This is a base renderer for mazegen.
     """
-    def render(self, maze_obj):
+    def render(self, maze_obj: maze.Maze) -> bool:
         """
         This method renders the maze.
         """
         print(maze_obj)
+
+        return True
+
+class RendererFactory:
+    """
+    This class is used for creating Renderer classes. 
+    """
+
+    def __init__(self) -> None:
+        pass
+
+    def create_renderer(self, args: str = "") -> BaseRenderer:
+        """
+        Parse the additional args and return Renderer object
+        """
+
+        print(args)
+        return BaseRenderer()
